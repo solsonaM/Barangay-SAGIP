99d4dd164f20f61152d859d3506cf9481fa430ba
Angelo Solsona <114324157+solsonaM@users.noreply.github.com>
2026-09-18 02:50:12 +0800

    refactor: remove resident home coordinate fields

--- barangay-sagip-web/app/Models/ResidentProfile.php
+++ barangay-sagip-web/app/Models/ResidentProfile.php
@@ -17,16 +17,12 @@ class ResidentProfile extends Model
         'household_members_count',
         'emergency_contact_name',
         'emergency_contact_number',
-        'home_latitude',
-        'home_longitude',
     ];
 
     protected function casts(): array
     {
         return [
             'birthdate' => 'date',
-            'home_latitude' => 'decimal:7',
-            'home_longitude' => 'decimal:7',
         ];
     }
 
